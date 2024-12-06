f06c4625a184d551d20969272390779cd57d26b6
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2024-12-06 17:16:11 +0800

    feat: add data pagination, sorting, filtering, grouping

---
+++ source/SingleScope.Collection/Queryable/Options/GroupOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace SingleScope.Collection.Queryable.Options
+{
+    public class GroupOptions
+    {
+        public required string Field { get; set; }
+
+        public string? Direction { get; set; }
+
+        public IEnumerable<AggregateOptions>? Aggregates { get; set; }
+    }
+}
---
+++ source/SingleScope.Core/Extensions/JsonElementExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Text.Json;
+
+namespace SingleScope.Core.Extensions
+{
+    public static class JsonElementExtensions
+    {
+        public static object? GetValueAsObject(this JsonElement element)
+        {
+            var kind = element.ValueKind;
+            var raw = element.GetRawText();
+
+            return kind switch
+            {
+                JsonValueKind.Number => float.Parse(raw, CultureInfo.InvariantCulture),
+                JsonValueKind.String => element.GetString(),
+                JsonValueKind.True => element.GetBoolean(),
+                JsonValueKind.False => element.GetBoolean(),
+                JsonValueKind.Null => null,
+                JsonValueKind.Undefined => null,
+                JsonValueKind.Object => raw,
+                JsonValueKind.Array => raw,
+                _ => raw
+            };
+        }
+    }
+}
