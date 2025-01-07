94c9ddabfc73d7a5b73b9378df3886f0b4c3f527
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2025-01-07 22:01:39 +0800

    feat: add navigation service for page navigation & shell navigation

---
+++ source/SingleScope.Maui/Navigation/IShellNavigationService.cs
@@ -0,0 +1,17 @@
+﻿namespace SingleScope.Maui.Navigation
+{
+    public interface IShellNavigationService
+    {
+        Task PushAsync<TView>(ShellNavigationQueryParameters? query = null) where TView : Page;
+
+        Task PopAsync(ShellNavigationQueryParameters? query = null);
+
+        Task PopToRootAsync<TRoot>(ShellNavigationQueryParameters? query = null) where TRoot : Page;
+
+        void Push<TView>(ShellNavigationQueryParameters? query = null) where TView : Page;
+
+        void Pop(ShellNavigationQueryParameters? query = null);
+
+        void PopToRoot<TRoot>(ShellNavigationQueryParameters? query = null) where TRoot : Page;
+    }
+}
---
+++ source/SingleScope.Maui/Navigation/ShellNavigationService.cs
@@ -0,0 +1,35 @@
+﻿namespace SingleScope.Maui.Navigation
+{
+    public class ShellNavigationService : IShellNavigationService
+    {
+        public void Pop(ShellNavigationQueryParameters? query = null)
+        {
+            _ = PopAsync(query);
+        }
+
+        public Task PopAsync(ShellNavigationQueryParameters? query = null)
+        {
+            return Shell.Current.GoToAsync("..", query);
+        }
+
+        public void PopToRoot<TRoot>(ShellNavigationQueryParameters? query = null) where TRoot : Page
+        {
+            _ = PopToRootAsync<TRoot>(query);
+        }
+
+        public Task PopToRootAsync<TRoot>(ShellNavigationQueryParameters? query = null) where TRoot : Page
+        {
+            return Shell.Current.GoToAsync($"///{nameof(TRoot)}", query);
+        }
+
+        public void Push<TView>(ShellNavigationQueryParameters? query = null) where TView : Page
+        {
+            _ = PushAsync<TView>(query);
+        }
+
+        public Task PushAsync<TView>(ShellNavigationQueryParameters? query = null) where TView : Page
+        {
+            return Shell.Current.GoToAsync(nameof(TView), query);
+        }
+    }
+}
