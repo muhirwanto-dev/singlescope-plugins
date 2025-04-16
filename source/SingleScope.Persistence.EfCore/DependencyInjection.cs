ddf997b0efcd336e11b3ba7611a2aa538b8af59a
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2025-04-16 11:07:07 +0800

    fix: specification evaluator not injected

--- source/SingleScope.Persistence.EfCore/DependencyInjection.cs
+++ source/SingleScope.Persistence.EfCore/DependencyInjection.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using SingleScope.Persistence.EFCore.Querying;
 using SingleScope.Persistence.EFCore.Repository;
 using SingleScope.Persistence.EFCore.UnitOfWork;
+using SingleScope.Persistence.Querying;
 using SingleScope.Persistence.Repository;
 using SingleScope.Persistence.UnitOfWork;
 
@@ -14,9 +16,10 @@ namespace SingleScope.Persistence.EFCore
             this IServiceCollection services
             )
         {
-            services.TryAddScoped(typeof(IRepository<,,>), typeof(ReadWriteRepository<,,>));
-            services.TryAddScoped(typeof(IReadRepository<,,>), typeof(ReadOnlyRepository<,,>));
-            services.TryAddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+            services.AddScoped(typeof(IRepository<,,>), typeof(ReadWriteRepository<,,>));
+            services.AddScoped(typeof(IReadRepository<,,>), typeof(ReadOnlyRepository<,,>));
+            services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+            services.AddSingleton<ISpecificationEvaluator, SpecificationEvaluator>();
 
             return services;
         }
@@ -30,6 +33,7 @@ namespace SingleScope.Persistence.EFCore
             services.TryAddScoped(typeof(IRepository<,,>), typeof(ReadWriteRepository<,,>));
             services.TryAddScoped(typeof(IReadRepository<,,>), typeof(ReadOnlyRepository<,,>));
             services.TryAddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+            services.TryAddSingleton<ISpecificationEvaluator, SpecificationEvaluator>();
 
             services.AddDbContext<TContext>(dbContextOptionsAction);
 
