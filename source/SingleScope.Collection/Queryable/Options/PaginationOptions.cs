ad7549d116c6e98cb341c9591ac001c7585c5b74
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2025-04-16 11:10:03 +0800

    root: SingleScope.Collection was moved to SingleScope.Common

--- source/SingleScope.Collection/Queryable/Options/PaginationOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SingleScope.Collection.Queryable.Options
-{
-    public class PaginationOptions
-    {
-        public int Page { get; set; }
-
-        public int PageSize { get; set; }
-    }
-}
--- source/SingleScope.Collection/Queryable/QueryResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SingleScope.Collection.Queryable
-{
-    public class QueryResult<T>
-    {
-        public ICollection<T>? Data { get; set; }
-
-        public int Total { get; set; }
-    }
-}
