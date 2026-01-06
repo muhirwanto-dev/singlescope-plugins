ef6592088c9553967d8f66b60df4dc731acad697
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2026-01-06 23:19:46 +0700

    feat: release Navigations for MAUI

--- source/SingleScope.Navigations.Maui/Extensions/ServiceCollectionExtensions.cs
+++ source/SingleScope.Navigations.Maui/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection.Extensions;
 using SingleScope.Navigations.Abstractions;
 using SingleScope.Navigations.Maui.Adapters;
+using SingleScope.Navigations.Maui.Options;
 using SingleScope.Navigations.Maui.Services;
 
 namespace SingleScope.Navigations.Maui.Extensions
@@ -12,7 +13,13 @@ namespace SingleScope.Navigations.Maui.Extensions
         /// </summary>
         public static IServiceCollection AddSingleScopeNavigations(
             this IServiceCollection services)
+            => services.AddSingleScopeNavigations(opt => { });
+
+        public static IServiceCollection AddSingleScopeNavigations(
+            this IServiceCollection services, Action<NavigationOptions> configuration)
         {
+            services.Configure(configuration);
+
             // Navigation adapters
             services.AddSingleton<INavigationAdapter, ShellNavigationAdapter>();
             services.AddSingleton<INavigationAdapter, PageNavigationAdapter>();
