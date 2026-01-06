feab89268aeec39cd332de101d8bae531da873a1
muhirwanto-dev <muhammadirwanto.dev@gmail.com>
2026-01-06 17:44:50 +0700

    fix: reporting options not injected to DI

--- source/SingleScope.Reporting.Maui/Extensions/ServiceCollectionExtensions.cs
+++ source/SingleScope.Reporting.Maui/Extensions/ServiceCollectionExtensions.cs
@@ -1,13 +1,20 @@
 ﻿using SingleScope.Reporting.Abstractions;
 using SingleScope.Reporting.Maui.Sinks;
+using SingleScope.Reporting.Options;
 
 namespace SingleScope.Reporting.Maui.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddSingleScopeReportingMaui(
-            this IServiceCollection services)
+        public static IServiceCollection AddSingleScopeReporting(
+            this IServiceCollection services) 
+            => services.AddSingleScopeReporting(options => { });
+
+        public static IServiceCollection AddSingleScopeReporting(
+            this IServiceCollection services, Action<ReportingOptions> options)
         {
+            services.Configure(options);
+
             return services
                 .AddSingleton<IReportSink, MauiDialogReportSink>();
         }
